6df1f8fd5e2565b2597f3a7fc90f40dc78edd8c8
Ed <59697268+Edpacca@users.noreply.github.com>
2020-09-03 21:33:56 +0100

    new project instantiated

---
+++ RightmoveScraperInterface/JsonRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RightmoveScraperInterface
+{
+    public class JsonRequest
+    {
+        private string SearchArea { get => _searchArea; set => _searchArea = value; }
+        private string _searchArea;
+
+        public Dictionary<string, int> JsonBasicSearchParameters = new Dictionary<string, int>();
+        public List<Dictionary<string, bool>> JsonAdvancedSearchParameters = new List<Dictionary<string, bool>>();
+
+        public JsonRequest(string searchArea)
+        {
+            SearchArea = searchArea;
+        }
+    }
+}
